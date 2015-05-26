e16fb30fdb83e2ca8a06cc1a307d8a42cbb17423
Michael <monolisa56@gmail.com>
2015-05-26 17:34:36 -0400

    first servlet example.

---
+++ first/src/com/javatpoint/examples/Controller.java
@@ -0,0 +1,42 @@
+package com.javatpoint.examples;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Controller
+ */
+@WebServlet(description = "servlet example form www.javatpoint.com", urlPatterns = { "/Controller" })
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Controller() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		getServletContext().getRequestDispatcher("/Welcome.jsp").forward(request, response);	
+	
+		/*
+		response.setContentType("text/html");//setting the content type  
+		PrintWriter pw=response.getWriter();//get the stream to write the data  
+		  
+		*/
+	}
+
+}
